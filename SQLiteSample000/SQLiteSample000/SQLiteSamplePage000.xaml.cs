c3d5409988b9f5342e488be4aaf74c5d13bfe2d1
謙太 可士村 <150365@jc-21.jp>
2017-11-21 15:36:55 +0900

    ハンドラついか

--- SQLiteSample000/SQLiteSample000/SQLiteSamplePage000.xaml.cs
+++ SQLiteSample000/SQLiteSample000/SQLiteSamplePage000.xaml.cs
@@ -8,7 +8,7 @@ namespace SQLiteSample000
         public SQLiteSamplePage000()
         {
             //InitializeComponent();
-
+           
             var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center, Margin = new Thickness { Top = 100 } };
 
             //Userテーブルに適当なデータを追加
@@ -16,14 +16,16 @@ namespace SQLiteSample000
             UserModel.insertUser("田中", "13");
             UserModel.insertUser("斎藤", "18");
 
-
+ 　　　　　　
 
             //Userテーブルの行データを取得
             var query = UserModel.selectUser();
 
+            Button button1 = null;
+
             foreach (var user in query)
             {
-                Button button1;
+                
                 //Userテーブルの名前列をLabelに書き出す
                 layout.Children.Add(button1 = new Button{ Text = user.Name });
             
@@ -32,7 +34,10 @@ namespace SQLiteSample000
 
             }
 
-
+           
+            button1.Clicked += new EventHandler(Button1_Click);
+            
+            
             void Button1_Click(object sender, EventArgs e)
             {
                 DisplayAlert("UserModel", "ボタンが押されたんご", "OK");//
